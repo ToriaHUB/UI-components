364ca85f6ce0d01361d842ed3134c8dfcce6252f
Viktoria Petrenko <vikuacz@seznam.cz>
2019-11-06 00:38:00 +0100

    master: added PlusIcon and DeleteIcon components

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,16 @@
 import React from "react"
 
 import "./App.css"
-import { Button } from "./components/Button"
 import styled from "styled-components"
+import { PlusIcon } from "./icons/PlusIcon"
+import { DeleteIcon } from "./icons/DeleteIcon"
 
 function App() {
   return (
-    <Content>
-      <ColumnContainer>
-        <ButtonTitleStyle>Normal state</ButtonTitleStyle>
-        <Button bgColor={"green"} isDisabled={false} buttonType={"big"} isReverse={false}>
-          Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={false} buttonType={"big"} isReverse={false}>
-          Red Button without Icon
-        </Button>
-        <Button bgColor={"green"} isDisabled={false} buttonType={"small"} isReverse={false}>
-          Small Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={false} buttonType={"small"} isReverse={false}>
-          Small Red Button without Icon
-        </Button>
-      </ColumnContainer>
-
-      <ColumnContainer>
-        <ButtonTitleStyle>Normal reverse state</ButtonTitleStyle>
-        <Button bgColor={"green"} isDisabled={false} buttonType={"big"} isReverse={true}>
-          Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={false} buttonType={"big"} isReverse={true}>
-          Red Button without Icon
-        </Button>
-        <Button bgColor={"green"} isDisabled={false} buttonType={"small"} isReverse={true}>
-          Small Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={false} buttonType={"small"} isReverse={true}>
-          Small Red Button without Icon
-        </Button>
-      </ColumnContainer>
-
-      <ColumnContainer>
-        <ButtonTitleStyle>Normal disabled state</ButtonTitleStyle>
-        <Button bgColor={"green"} isDisabled={true} buttonType={"big"} isReverse={false}>
-          Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={true} buttonType={"big"} isReverse={false}>
-          Red Button without Icon
-        </Button>
-        <Button bgColor={"green"} isDisabled={true} buttonType={"small"} isReverse={false}>
-          Small Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={true} buttonType={"small"} isReverse={false}>
-          Small Red Button without Icon
-        </Button>
-      </ColumnContainer>
-
-      <ColumnContainer>
-        <ButtonTitleStyle>Reverse disabled state</ButtonTitleStyle>
-        <Button bgColor={"green"} isDisabled={true} buttonType={"big"} isReverse={true}>
-          Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={true} buttonType={"big"} isReverse={true}>
-          Red Button without Icon
-        </Button>
-        <Button bgColor={"green"} isDisabled={true} buttonType={"small"} isReverse={true}>
-          Small Button without Icon
-        </Button>
-        <Button bgColor={"red"} isDisabled={true} buttonType={"small"} isReverse={true}>
-          Small Red Button without Icon
-        </Button>
-      </ColumnContainer>
-    </Content>
+    <div>
+      <PlusIcon size={"big"} />
+      <DeleteIcon size={"big"} />
+    </div>
   )
 }
 
---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+type Props = {
+  size: "big" | "small"
+  color?: string
+}
+
+export const PlusIcon = (props: Props) => {
+  const { size, color } = props
+  return (
+    <svg
+      width={size === "big" ? "20px" : "10px"}
+      height={size === "big" ? "20px" : "10px"}
+      fill={props.color}
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 50 50"
+      enableBackground="new 0 0 50 50"
+      xmlSpace={"preserve"}
+    >
+      <path
+        d="M9.077,25.99h14v14c0,0.553,0.448,1,1,1s1-0.447,1-1v-14h14c0.552,0,1-0.447,1-1s-0.448-1-1-1h-14v-14c0-0.553-0.448-1-1-1
+	s-1,0.447-1,1v14h-14c-0.552,0-1,0.447-1,1S8.525,25.99,9.077,25.99z"
+      />
+    </svg>
+  )
+}
